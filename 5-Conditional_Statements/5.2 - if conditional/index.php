37aa9ef86e91a6464de52354224789d08064e054
jayden van der bijl <2158035@talnet.nl>
2022-02-02 09:47:09 +0100

    opdracht5.2

--- 5-Conditional_Statements/5.2 - if conditional/index.php
+++ 5-Conditional_Statements/5.2 - if conditional/index.php
@@ -1,5 +1,14 @@
 <?php
-    $variabele1 = 10;
-    $variabele2 = 10;
-    echo $variabele1 == $variabele2;
+    $test1 = 10;
+    $test2 = 6;
+    
+    if ($test1 == $test2) echo "gelijk";
+    if ($test1 != $test2) echo "ongelijk";
+    if ($test1 === $test2) echo "identiek";
+    echo "</br>";
+    if ($test1 > $test2) echo "test1 is groter dan test2";
+    if ($test1 < $test2) echo "test1 is kleiner dan test2";
+    echo "</br>";
+    if ($test1 >= $test2) echo "test1 is groter of gelijk aan test2";
+    if ($test1 <= $test2) echo "test1 is kleiner of gelijk aan test2";
 ?>
