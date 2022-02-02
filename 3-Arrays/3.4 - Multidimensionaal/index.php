7cb654c7f9ddc3a8feee81c2c10201e6af4c15b3
jayden van der bijl <2158035@talnet.nl>
2022-02-02 14:25:53 +0100

    opdracht3.4

--- 3-Arrays/3.4 - Multidimensionaal/index.php
+++ 3-Arrays/3.4 - Multidimensionaal/index.php
@@ -1,5 +1,7 @@
 <?php
-     $complexeLijst = [ [1,2,3], ["een", "twee", "drie"], true, false  ];
-
-     echo $complexeLijst[0][1];
+$abcGetallen=[ ["a","b","c","d"], [24,56,78,90] ];
+echo $abcGetallen[0][0];
+echo $abcGetallen[1][1] . "<br>";
+echo $abcGetallen[0][2];
+echo $abcGetallen[1][3];
 ?>
